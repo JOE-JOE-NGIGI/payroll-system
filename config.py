72ca6ad220dddfca3f0790ebff14a5ed4b814e98
JOE-JOE-NGIGI <jngigiw@gmail.com>
2019-08-08 12:39:47 +0300

    refine

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
 
 class Development(Config):
     SQLALCHEMY_DATABASE_URI = 'postgres://postgres:data@*0418@127.0.0.1:5432/july_payroll'
-    environment = 'Developmet'
+    #environment = 'Developmet'
     DEBUG = True
 
 class Testing(Config):
@@ -19,5 +19,5 @@ class Testing(Config):
 class Production(Config):
     SQLALCHEMY_DATABASE_URI = 'postgres://ctpjkgbceszgun:0ae8b5c5e8cfc16245878c66f0cfe275b8ad78ebbb7a5d1c22e0828716b6e68b@ec2-54-221-215-228.compute-1.amazonaws.com:5432/dfq0lifq3j8974'
     DEBUG = False
-    #environment = 'Production'
+    environment = 'Production'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
