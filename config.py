6cca699786f13521ef573422ce1d5305f9912821
JOE-JOE-NGIGI <jngigiw@gmail.com>
2019-08-08 10:29:29 +0300

    Fix UI bugs

--- config.py
+++ config.py
@@ -19,5 +19,5 @@ class Testing(Config):
 class Production(Config):
     SQLALCHEMY_DATABASE_URI = 'postgres://ctpjkgbceszgun:0ae8b5c5e8cfc16245878c66f0cfe275b8ad78ebbb7a5d1c22e0828716b6e68b@ec2-54-221-215-228.compute-1.amazonaws.com:5432/dfq0lifq3j8974'
     DEBUG = False
-    # environment = 'Production'
+    #environment = 'Production'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
